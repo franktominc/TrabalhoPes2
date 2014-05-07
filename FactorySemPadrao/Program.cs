ec66bf9966f71ed868da3f9677cad0fa4272f960
Frank P. Tominc <frankptominc@gmail.com>
2014-05-07 09:25:38 -0300

    Factory Sem Padrão Adicionado

---
+++ FactorySemPadrao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FactorySemPadrao {
+    class Program {
+        static void Main(string[] args) {
+            Eletronico Computador = new Computador("I7", "8GB");
+            Console.WriteLine(Computador);
+            Eletronico Calculadora = new Calculadora(15);
+            Console.WriteLine(Calculadora);
+            Eletronico Notebook = new Notebook("I7", "6GB", "15 Polegadas");
+            Console.WriteLine(Notebook);
+            Console.ReadKey();
+        }
+    }
+}
