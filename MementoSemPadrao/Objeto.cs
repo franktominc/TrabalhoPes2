2a115242a4a33e0e6ec57affc4be1d5a5f39db8f
Frank P. Tominc <frankptominc@gmail.com>
2014-05-07 10:07:50 -0300

    Memento sem padrão adicionado

---
+++ MementoSemPadrao/Objeto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoSemPadrao {
+    public class Objeto {
+        public Estado EstadoAtual { get; set; }
+        public void MudaEstado(Estado NovoEstado) {
+            EstadoAtual = NovoEstado;
+        }
+    }
+}
