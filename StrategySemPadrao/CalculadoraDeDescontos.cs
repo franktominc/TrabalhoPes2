b320dd0b1276e85c0aef1ebe2c961db1916db0f1
Frank P. Tominc <frankptominc@gmail.com>
2014-05-07 06:25:46 -0300

    Strategy Sem Padrão Adicionado

---
+++ StrategySemPadrao/CalculadoraDeDescontos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategySemPadrao {
+    public class CalculadoraDeDescontos {
+        public double AplicaDescontoDiaDasMaes(Venda v) {
+            return v.Valor * 0.07;
+        }
+        public double AplicaDescontoClienteEspecial(Venda v) {
+            return v.Valor * 0.1;
+        }
+        public double AplicaDescontoFuncionario(Venda v) {
+            return v.Valor * 0.2;
+        }
+        public double AplicaDescontoNatal(Venda v) {
+            return v.Valor * 0.05;
+        }
+    }
+}
---
+++ StrategySemPadrao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategySemPadrao {
+    class Program {
+        static void Main(string[] args) {
+            Venda v = new Venda(500);
+            CalculadoraDeDescontos calc = new CalculadoraDeDescontos();
+            Console.WriteLine(calc.AplicaDescontoClienteEspecial(v));
+            Console.WriteLine(calc.AplicaDescontoDiaDasMaes(v));
+            Console.WriteLine(calc.AplicaDescontoFuncionario(v));
+            Console.WriteLine(calc.AplicaDescontoNatal(v));
+        }
+    }
+}
