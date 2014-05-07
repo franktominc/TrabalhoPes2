6d3a2b44aa2b0504b782ab4de87cfb1cd04681a9
Frank P. Tominc <frankptominc@gmail.com>
2014-05-07 09:28:34 -0300

    Sincronizando

---
+++ Factory/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Eletronico el = EletronicosFactory.getEletronico("Computador");
+            Console.WriteLine(el);
+            el = EletronicosFactory.getEletronico("Notebook");
+            Console.WriteLine(el);
+            el = EletronicosFactory.getEletronico("Calculadora");
+            Console.WriteLine(el);
+            Console.ReadKey();
+        }
+    }
+}
