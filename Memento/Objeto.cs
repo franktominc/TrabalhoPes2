54dbee3dcd5ac9bb591892afbbe106741d7b0101
Frank P. Tominc <frankptominc@gmail.com>
2014-05-07 09:55:02 -0300

    Memento Adicionado

---
+++ Memento/Objeto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento {
+    public class Objeto {
+        private Stack<Estado> Estados = new Stack<Estado>();
+        public Estado EstadoAtual { get; private set; }
+        public void MudaEstado(Estado novoEstado) {
+            Estados.Push(novoEstado);
+            EstadoAtual = novoEstado;
+        }
+        public void VoltaEstado() {
+            if (!(Estados.Count == 0)) {
+                Console.WriteLine("Voltando para o estado " + Estados.Peek());
+                EstadoAtual = Estados.Peek();
+                Estados.Pop();
+            } else {
+                Console.WriteLine("Não existe estado anterior");
+            }
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento {
+    class Program {
+        static void Main(string[] args) {
+            Objeto objeto = new Objeto();
+            objeto.MudaEstado(new Estado1());
+            objeto.MudaEstado(new Estado2());
+            objeto.MudaEstado(new Estado3());
+            Console.WriteLine("Estado Atual "+ objeto.EstadoAtual);
+            objeto.VoltaEstado();
+            objeto.VoltaEstado();
+            objeto.VoltaEstado();
+            objeto.VoltaEstado();
+            Console.ReadKey();
+        }
+    }
+}
