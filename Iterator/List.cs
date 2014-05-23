ceccfdbbf28cf40c78f44ac5bd8c8be3d4482ec7
Frank P. Tominc <frankptominc@gmail.com>
2014-05-23 00:51:18 -0300

    Iterator com padrão adicionado

---
+++ Iterator/List.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator {
+    public class List : IList {
+        public NoDE Start { get;private set; }
+        public IIterator GetIterator() {
+            return new ListIterator(this);
+        }
+
+        public void insert(int dado) {
+            Start = new NoDE(Start,null, dado);
+        }
+    }
+
+    class ListIterator : IIterator {
+        private NoDE currentNoDe;
+
+        public ListIterator(List list) {
+            currentNoDe = list.Start;
+        }
+
+        public bool HasNext() {
+            return currentNoDe != null;
+        }
+
+        public object Next() {
+            if (HasNext()) {
+                int aux = currentNoDe.Dado;
+                currentNoDe = currentNoDe.ProxNoDe;
+                return aux;
+            }
+            return null;
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator {
+    class Program {
+        static void Main(String[] Args) {
+            List list = new List();
+            list.insert(10);
+            list.insert(20);
+            list.insert(30);
+            IIterator it = list.GetIterator();
+            while (it.HasNext()) {
+                Console.WriteLine(it.Next());
+            }
+            Console.ReadKey();
+        }
+    }
+}
